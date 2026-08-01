db951cd52b97e9613d77e8dea7b6eb5a61d47d3e
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-01 16:16:54 +0530

    173 Binary Search Tree Iterator - Runtime: 1 ms, Memory: 31.9 MB

---
+++ 173-binary-search-tree-iterator.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+    BSTIterator(TreeNode* root) {
+        while (root != NULL) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+    int next() {
+        if (st.empty()) {
+            return 1 - 1;
+        }
+        TreeNode* top_node = st.top();
+        st.pop();
+        TreeNode* right_node = top_node->right;
+        while (right_node != NULL) {
+            st.push(right_node);
+            right_node = right_node->left;
+        }
+        return top_node->val;
+    }
+    bool hasNext() { return !st.empty(); }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
