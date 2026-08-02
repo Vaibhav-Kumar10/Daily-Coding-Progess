2662a1b742913a43b4527b7aa21e0f6ce839afc9
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-02 11:41:43 +0530

    486 Predict the Winner - Runtime: 0 ms, Memory: 10.3 MB

---
+++ 486-predict-the-winner.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int f(int left, int right, vector<int>& nums, int n,
+          vector<vector<int>>& dp) {
+        if (left > right) {
+            return 0;
+        }
+
+        if (dp[left][right] != -1) {
+            return dp[left][right];
+        }
+        // Player 1 take left
+        int take_left = nums[left];
+        int l_p2_take_left = f(left + 2, right, nums, n, dp);
+        int l_p2_take_right = f(left + 1, right - 1, nums, n, dp);
+        take_left += min(l_p2_take_left, l_p2_take_right);
+
+        // Player 1 take right
+        int take_right = nums[right];
+        int r_p2_take_left = f(left + 1, right - 1, nums, n, dp);
+        int r_p2_take_right = f(left, right - 2, nums, n, dp);
+        take_right += min(r_p2_take_left, r_p2_take_right);
+
+        return dp[left][right] = max(take_left, take_right);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size(), total_score = 0;
+        for (int stone : nums) {
+            total_score += stone;
+        }
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        int p1_score = f(0, n - 1, nums, n, dp);
+        int p2_score = total_score - p1_score;
+        return p1_score >= p2_score;
+    }
+};
