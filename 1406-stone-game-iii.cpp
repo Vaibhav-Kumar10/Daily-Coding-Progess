a68f0de1bd407138fb3a715dbe78f2b40033346f
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-03 16:47:00 +0530

    1406 Stone Game III - Runtime: 11 ms, Memory: 136.3 MB

--- 1406-stone-game-iii.cpp
+++ 1406-stone-game-iii.cpp
@@ -1,5 +1,51 @@
 class Solution {
 public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int total_score = 0, n = stoneValue.size();
+        for (int val : stoneValue) {
+            total_score += val;
+        }
+        vector<int> dp(n + 1, 0);
+        for (int ind = n - 1; ind >= 0; ind--) {
+            // Alice takes 1 stone
+            int alice_one_stone = stoneValue[ind];
+            // Bob after Alice takes 1 stone
+            int bob_after_one_stone = dp[ind + 1];
+            int score_dif = alice_one_stone - bob_after_one_stone;
+
+            // Alice takes 2 stones
+            if (ind + 2 <= n) {
+                int alice_two_stone = stoneValue[ind] + stoneValue[ind + 1];
+                // Bob after Alice takes 2 stones
+                int bob_after_two_stone = dp[ind + 2];
+
+                score_dif =
+                    max(score_dif, alice_two_stone - bob_after_two_stone);
+            }
+
+            // Alice takes 3 stones
+            if (ind + 3 <= n) {
+                int alice_three_stone =
+                    stoneValue[ind] + stoneValue[ind + 1] + stoneValue[ind + 2];
+                // Bob after Alice takes 3 stones
+                int bob_after_three_stone = dp[ind + 3];
+
+                score_dif =
+                    max(score_dif, alice_three_stone - bob_after_three_stone);
+            }
+
+            dp[ind] = score_dif;
+        }
+
+        int alice_bob_score = dp[0];
+        if (alice_bob_score > 0) {
+            return "Alice";
+        } else if (alice_bob_score < 0) {
+            return "Bob";
+        }
+        return "Tie";
+    }
+    /*
     int f(int ind, vector<int>& stoneValue, int n, vector<int>& dp) {
         if (ind >= n) {
             return 0;
@@ -50,4 +96,5 @@ public:
         }
         return "Tie";
     }
+    */
 };
