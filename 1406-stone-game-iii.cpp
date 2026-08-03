04e7950ccb992149dee8df2523f96be3a2db3e90
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-03 16:50:32 +0530

    1406 Stone Game III - Runtime: 5 ms, Memory: 130.3 MB

--- 1406-stone-game-iii.cpp
+++ 1406-stone-game-iii.cpp
@@ -1,10 +1,50 @@
 class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
-        int total_score = 0, n = stoneValue.size();
-        for (int val : stoneValue) {
-            total_score += val;
+        int n = stoneValue.size(), a_1 = 0, a_2 = 0, a_3 = 0;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            // Alice takes 1 stone
+            int alice_one_stone = stoneValue[ind];
+            // Bob after Alice takes 1 stone
+            int bob_after_one_stone = a_1;
+            int score_dif = alice_one_stone - bob_after_one_stone;
+
+            // Alice takes 2 stones
+            if (ind + 2 <= n) {
+                int alice_two_stone = stoneValue[ind] + stoneValue[ind + 1];
+                // Bob after Alice takes 2 stones
+                int bob_after_two_stone = a_2;
+
+                score_dif =
+                    max(score_dif, alice_two_stone - bob_after_two_stone);
+            }
+
+            // Alice takes 3 stones
+            if (ind + 3 <= n) {
+                int alice_three_stone =
+                    stoneValue[ind] + stoneValue[ind + 1] + stoneValue[ind + 2];
+                // Bob after Alice takes 3 stones
+                int bob_after_three_stone = a_3;
+
+                score_dif =
+                    max(score_dif, alice_three_stone - bob_after_three_stone);
+            }
+            a_3 = a_2;
+            a_2 = a_1;
+            a_1 = score_dif;
         }
+
+        int alice_bob_score = a_1;
+        if (alice_bob_score > 0) {
+            return "Alice";
+        } else if (alice_bob_score < 0) {
+            return "Bob";
+        }
+        return "Tie";
+    }
+    /*
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
         vector<int> dp(n + 1, 0);
         for (int ind = n - 1; ind >= 0; ind--) {
             // Alice takes 1 stone
@@ -45,6 +85,7 @@ public:
         }
         return "Tie";
     }
+    */
     /*
     int f(int ind, vector<int>& stoneValue, int n, vector<int>& dp) {
         if (ind >= n) {
@@ -83,10 +124,7 @@ public:
         return dp[ind] = score_dif;
     }
     string stoneGameIII(vector<int>& stoneValue) {
-        int total_score = 0, n = stoneValue.size();
-        for (int val : stoneValue) {
-            total_score += val;
-        }
+        int n = stoneValue.size();
         vector<int> dp(n, INT_MIN);
         int alice_bob_score = f(0, stoneValue, n, dp);
         if (alice_bob_score > 0) {
