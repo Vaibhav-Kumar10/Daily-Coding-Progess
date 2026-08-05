69b9c858928b0d6275495e90c9d33289fd341801
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-05 14:47:43 +0530

    62 Unique Paths - Runtime: 0 ms, Memory: 9.5 MB

--- 62-unique-paths.cpp
+++ 62-unique-paths.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0] = 1;
+        for (int cur_x = 0; cur_x < m; cur_x++) {
+            for (int cur_y = 0; cur_y < n; cur_y++) {
+                if (cur_x == 0 && cur_y == 0) {
+                    continue;
+                }
+                int up = 0, left = 0;
+                if (cur_x > 0) {
+                    up += dp[cur_x - 1][cur_y];
+                }
+                if (cur_y > 0) {
+                    left += dp[cur_x][cur_y - 1];
+                }
+                dp[cur_x][cur_y] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    /*
     int f(int cur_x, int cur_y, int m, int n, vector<vector<int>>& dp) {
         if (cur_x == m - 1 && cur_y == n - 1) {
             return 1;
@@ -20,4 +41,5 @@ public:
         vector<vector<int>> dp(m, vector<int>(n, -1));
         return f(0, 0, m, n, dp);
     }
+    */
 };
