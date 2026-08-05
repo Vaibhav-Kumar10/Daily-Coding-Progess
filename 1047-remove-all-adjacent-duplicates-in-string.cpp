3ce95189a44a9d25c2f2cdbc264ab2e7ede23222
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-05 23:44:48 +0530

    1047 Remove All Adjacent Duplicates In String - Runtime: 6 ms, Memory: 16.1 MB

---
+++ 1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<int> st;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (st.empty()) {
+                st.push(i);
+            } else if (s[st.top()] == s[i]) {
+                st.pop();
+            } else {
+                st.push(i);
+            }
+        }
+        string ans;
+        while (!st.empty()) {
+            ans += s[st.top()];
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
