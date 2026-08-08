a1ff2c3d3783affaa500fd3d81fbfde2ec7b9bae
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-08 11:40:08 +0530

    1027 Longest Arithmetic Subsequence - Runtime: 102 ms, Memory: 145.4 MB

--- 1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    int longestArithSeqLength(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 2) {
+            return n;
+        }
+        int ans = 0;
+        // -ve dif also possible
+        vector<vector<int>> dp(n, vector<int>(1001, 1));
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int dif = nums[i] - nums[j];
+                // adding offset bcz => -ve dif also possible
+                int d = dif + 500;
+                dp[i][d] = max(dp[i][d], dp[j][d] + 1);
+                ans = max(ans, dp[i][d]);
+            }
+        }
+        return ans;
+    }
+    /*
     int f(int ind, int dif, vector<int>& nums, vector<vector<int>>& dp) {
         if (ind < 0) {
             return 0;
@@ -32,4 +52,5 @@ public:
         }
         return ans;
     }
+    */
 };
