fb382d5102d14715f438d49defb96c36ec826831
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-09 12:31:39 +0530

    4015 Weighted Sum of a Tree - Runtime: 119 ms, Memory: 239.7 MB

--- 4015-weighted-sum-of-a-tree.cpp
+++ 4015-weighted-sum-of-a-tree.cpp
@@ -8,7 +8,9 @@ public:
         }
         int height = 0;
         queue<int> q;
+        vector<int> levels(n, 0);
         q.push(0);
+        levels[0] = 1;
         while (!q.empty()) {
             int q_size = q.size();
             while (q_size--) {
@@ -16,20 +18,14 @@ public:
                 q.pop();
                 for (int child : adj[node]) {
                     q.push(child);
+                    levels[child] = levels[node] + 1;
                 }
             }
             height++;
         }
-        queue<pair<int, int>> q2;
-        q2.push({0, 1});
         long long ans = 0;
-        while (!q2.empty()) {
-            auto [node, level] = q2.front();
-            q2.pop();
-            ans = ans + nums[node] * 1LL * (height - level + 1);
-            for (int child : adj[node]) {
-                q2.push({child, level + 1});
-            }
+        for (int node = 0; node < n; node++) {
+            ans = ans + nums[node] * 1LL * (height - levels[node] + 1);
         }
         return ans;
     }
