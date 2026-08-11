502c862624a45054a5450f4ed6d161cc7437e0ce
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-11 12:34:45 +0530

    1510 Stone Game IV - Runtime: 45 ms, Memory: 18.3 MB

--- 1510-stone-game-iv.cpp
+++ 1510-stone-game-iv.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
-    // 0 = Not calculated
-    // 1 = Alice
-    // 2 = Bob
-    std::vector<std::vector<int>> memo;
-    bool winnerSquareGame(int n) {
-        memo = std::vector<std::vector<int>>(n + 1, std::vector<int>(2, 0));
-        return game(n, true);
-    }
-
-    bool game(int n, bool isAlice) {
-        int isAliceIdx = isAlice ? 1 : 0;
-        if (memo[n][isAliceIdx] != 0) return memo[n][isAliceIdx] == 1;
-        int c = 1;
-        bool best = !isAlice;
-        int i = 1;
-        while (c <= n) {
-            if (!isAlice) {
-                if (!game(n - c, true)) {
-                    best = false;
-                    break;
-                }
-            } else {
-                if (game(n - c, false)) {
-                    best = true;
-                    break;
-                }
-            }
-            i++;
-            c = i * i;
+    bool canWin(int available_stones, vector<int>& dp) {
+        if (available_stones == 0) {
+            return false;
         }
-        memo[n][isAliceIdx] = best ? 1 : 2;
-        return best;
+        if (dp[available_stones] != -1) {
+            return dp[available_stones];
+        }
+        for (int possible_stones = 1;
+             possible_stones * possible_stones <= available_stones;
+             possible_stones++) {
+            // Check if opponent can win,
+            // when current player takes possible_stones^2 stones
+            if (canWin(available_stones - possible_stones * possible_stones,
+                       dp) == false) {
+                // Current player wins
+                return dp[available_stones] = true;
+            }
+        }
+        // Current player can't win
+        return dp[available_stones] = false;
     }
+    bool winnerSquareGame(int n) {
+        int available_stones = n;
+        vector<int> dp(n + 1, -1);
+        // Call for Alice
+        return canWin(available_stones, dp);
+    }
+    /*
+    bool canWin(int available_stones) {
+        if (available_stones == 0) {
+            return false;
+        }
+        for (int possible_stones = 1;
+             possible_stones * possible_stones <= available_stones;
+             possible_stones++) {
+            // Check if opponent can win,
+            // when current player takes possible_stones^2 stones
+            if (canWin(available_stones - possible_stones * possible_stones) ==
+                false) {
+                // Current player wins
+                return true;
+            }
+        }
+        // Current player can't win
+        return false;
+    }
+    bool winnerSquareGame(int n) {
+        int available_stones = n;
+        // Call for Alice
+        return canWin(available_stones);
+    }
+    */
 };
