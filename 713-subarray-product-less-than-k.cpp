50c0533a30cae16cef1ec1c698bd49ce396dde75
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-12 16:05:54 +0530

    713 Subarray Product Less Than K - Runtime: 6 ms, Memory: 44.7 MB

---
+++ 713-subarray-product-less-than-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k == 0) {
+            return 0;
+        }
+        int l = 0, r = 0, n = nums.size(), sub_arr_cnt = 0;
+        long long cur_prod = 1;
+        for (r; r < n; r++) {
+            cur_prod *= nums[r];
+            while (l <= r && cur_prod >= k) {
+                cur_prod /= nums[l];
+                l++;
+            }
+            sub_arr_cnt += (r - l + 1);
+        }
+        return sub_arr_cnt;
+    }
+};
