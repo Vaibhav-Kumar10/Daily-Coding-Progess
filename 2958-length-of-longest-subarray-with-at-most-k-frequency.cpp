5bd50eaad9f9b10fedcca3cd45bba2c0b9475ce2
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-12 11:03:53 +0530

    2958 Length of Longest Subarray With at Most K Frequency - Runtime: 63 ms, Memory: 149.4 MB

--- 2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int l = 0, r = 0, n = nums.size(), maxLen = 0, culprit = 0;
+        while (r < n) {
+            freq[nums[r]]++;
+            if (freq[nums[r]] == k + 1) {
+                culprit++;
+            }
+            if (culprit > 0) {
+                freq[nums[l]]--;
+                if (freq[nums[l]] == k) {
+                    culprit--;
+                }
+                l++;
+            }
+            if (culprit == 0) {
+                maxLen = max(maxLen, r - l + 1);
+            }
+            r++;
+        }
+        return maxLen;
+    }
+    /*
+    int maxSubarrayLength(vector<int>& nums, int k) {
         int l = 0, r = 0, n = nums.size(), maxLen = 0;
         unordered_map<int, int> freq;
         while (r < n) {
@@ -14,4 +37,5 @@ public:
         }
         return maxLen;
     }
+    */
 };
