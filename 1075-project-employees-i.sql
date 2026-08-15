8b402acf5ad436ae1a1bca52b1eda1f20c315f98
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-16 00:52:11 +0530

    1075 Project Employees I - Runtime: 585 ms, Memory: 0B

---
+++ 1075-project-employees-i.sql
@@ -0,0 +1,11 @@
+SELECT 
+    project_id,
+    IFNULL(ROUND(SUM(e.experience_years) / COUNT(p.employee_id), 2), 0) AS average_years
+FROM
+    Project AS p
+LEFT JOIN 
+    Employee AS e
+ON 
+    p.employee_id = e.employee_id
+GROUP BY
+    p.project_id;
