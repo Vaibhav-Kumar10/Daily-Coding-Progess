80d52d73ac6acab4dfb372fd21d8f15de7d7b443
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-17 11:39:59 +0530

    1563 Stone Game V - Runtime: 550 ms, Memory: 27.7 MB

--- 1563-stone-game-v.cpp
+++ 1563-stone-game-v.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int left = n - 1; left >= 0; left--) {
+            int total_sum = stoneValue[left];
+            for (int right = left + 1; right < n; right++) {
+                if (left == right) {
+                    continue;
+                }
+                total_sum += stoneValue[right];
+                int cur_sum = 0, ans = 0;
+                for (int i = left; i < right; i++) {
+                    cur_sum += stoneValue[i];
+                    int rem_sum = total_sum - cur_sum;
+                    if (cur_sum > rem_sum) {
+                        ans = max(ans, rem_sum + dp[i + 1][right]);
+                    } else if (cur_sum < rem_sum) {
+                        ans = max(ans, cur_sum + dp[left][i]);
+                    } else {
+                        ans = max(ans, rem_sum + dp[i + 1][right]);
+                        ans = max(ans, cur_sum + dp[left][i]);
+                    }
+                }
+                dp[left][right] = ans;
+            }
+        }
+        return dp[0][n - 1];
+    }
+    /*
     int dp[502][502];
     int f(int left, int right, int total_sum, vector<int>& stoneValue) {
         if (left == right) {
@@ -29,4 +58,5 @@ public:
         memset(dp, -1, sizeof(dp));
         return f(left, right, total_sum, stoneValue);
     }
+    */
 };
