ddb17f49c77f6ef07dee2c71114d34ad2243fbe5
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-19 11:17:43 +0530

    1031 Maximum Sum of Two Non-Overlapping Subarrays - Runtime: 0 ms, Memory: 11.9 MB

--- 1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    int f(vector<int>& nums, int l_len, int r_len) {
+        int n = nums.size(), r_block_sum = 0, l_block_sum = 0;
+        // Left block: [0, l_len - 1]
+        for (int i = 0; i < l_len; i++) {
+            l_block_sum += nums[i];
+        }
+        // Right block: [l_len, l_len + r_len - 1]
+        for (int i = l_len; i < l_len + r_len; i++) {
+            r_block_sum += nums[i];
+        }
+        int ans = l_block_sum + r_block_sum, maxLeftSubArrSum = l_block_sum;
+
+        for (int r_end = l_len + r_len; r_end < n; r_end++) {
+            int l_end = r_end - r_len;
+            int l_start_prev = l_end - l_len;
+
+            // Left block moves one position right
+            l_block_sum += nums[l_end] - nums[l_start_prev];
+
+            // Right block moves one position right
+            r_block_sum += nums[r_end] - nums[r_end - r_len];
+
+            maxLeftSubArrSum = max(maxLeftSubArrSum, l_block_sum);
+            ans = max(ans, maxLeftSubArrSum + r_block_sum);
+        }
+        return ans;
+    }
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        int n = nums.size();
+        int l_before_m = f(nums, firstLen, secondLen);
+        int m_before_l = f(nums, secondLen, firstLen);
+        return max(l_before_m, m_before_l);
+    }
+    /*
     int f(vector<int>& nums, vector<int>& prefixSum, int l_len, int r_len) {
         int n = nums.size(), ans = 0, maxLeftSubArrSum = 0;
         for (int r_end = l_len + r_len - 1; r_end < n; r_end++) {
@@ -24,4 +58,5 @@ public:
         int m_before_l = f(nums, prefixSum, secondLen, firstLen);
         return max(l_before_m, m_before_l);
     }
+    */
 };
