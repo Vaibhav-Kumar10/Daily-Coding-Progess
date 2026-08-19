d66fa78d7fe1167712938c7c815b9eaba268b091
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-19 12:49:17 +0530

    1386 Cinema Seat Allocation - Runtime: 49 ms, Memory: 66.8 MB

--- 1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int, int> occupiedGrpInRow;
+        for (auto row_res_seat : reservedSeats) {
+            int row = row_res_seat[0], seat = row_res_seat[1];
+            occupiedGrpInRow[row] |= (1 << seat);
+        }
+        int nonEmptyRows = occupiedGrpInRow.size();
+        int emptyRows = n - nonEmptyRows;
+        // For empty row => Group 1 and Group 2 can be booked
+        int maxGrps = 2 * emptyRows;
+
+        // Bit masks for Group 1: seats 2,3,4,5
+        int grp_1_mask = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
+        // Bit masks for Group 2: seats 4,5,6,7
+        int grp_2_mask = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+        // Bit masks for Group 3: seats 6,7,8,9
+        int grp_3_mask = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
+
+        for (auto [row, grpOccupiedMask] : occupiedGrpInRow) {
+            bool grp_1_possible = (grp_1_mask & grpOccupiedMask) == 0;
+            bool grp_2_possible = (grp_2_mask & grpOccupiedMask) == 0;
+            bool grp_3_possible = (grp_3_mask & grpOccupiedMask) == 0;
+            // Group 1 and Group 3 can be booked
+            if (grp_1_possible && grp_3_possible) {
+                maxGrps += 2;
+            }
+            // Only one of the Gropus 1, 2, and 3 can be booked
+            else if (grp_1_possible || grp_2_possible || grp_3_possible) {
+                maxGrps += 1;
+            }
+        }
+        return maxGrps;
+    }
+    /*
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         unordered_map<int, vector<bool>> occupiedGrpInRow;
         for (auto row_res_seat : reservedSeats) {
             int row = row_res_seat[0], seat = row_res_seat[1];
@@ -25,7 +59,7 @@ public:
         // For empty row => Group 1 and Group 2 can be booked
         int maxGrps = 2 * emptyRows;
         for (auto [row, grpOccupied] : occupiedGrpInRow) {
-            // Group 1 and Group 2 can be booked
+            // Group 1 and Group 3 can be booked
             if (grpOccupied[0] == false && grpOccupied[2] == false) {
                 maxGrps += 2;
             }
@@ -36,4 +70,5 @@ public:
         }
         return maxGrps;
     }
+    */
 };
