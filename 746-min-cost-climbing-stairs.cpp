7b846801a42bda84920920fbe6c0f7f7a94786ab
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-20 12:41:49 +0530

    746 Min Cost Climbing Stairs - Runtime: 3 ms, Memory: 17.6 MB

---
+++ 746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int f(int ind, vector<int>& cost, int n, vector<int>& dp) {
+        if (ind >= n) {
+            return 0;
+        }
+        if (dp[ind] != -1) {
+            return dp[ind];
+        }
+        // one step
+        int one_step = cost[ind] + f(ind + 1, cost, n, dp);
+        // two step
+        int two_step = cost[ind] + f(ind + 2, cost, n, dp);
+
+        return dp[ind] = min(one_step, two_step);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1, -1);
+        return min(f(0, cost, n, dp), f(1, cost, n, dp));
+    }
+};
