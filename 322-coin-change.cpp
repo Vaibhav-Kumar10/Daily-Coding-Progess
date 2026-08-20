26ca72d2d239ba24a355b58e3744dbffbd39e674
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-20 16:12:06 +0530

    322 Coin Change - Runtime: 48 ms, Memory: 48.9 MB

--- 322-coin-change.cpp
+++ 322-coin-change.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+        for (int amnt = 0; amnt <= amount; amnt++) {
+            // can first coin be used for remaining amount
+            if (amnt % coins[0] == 0) {
+                dp[0][amnt] = amnt / coins[0];
+            } else {
+                dp[0][amnt] = 1e9;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+                // Skip current coin
+                int not_take = dp[i - 1][t];
+                // Use current coin once
+                int take = t >= coins[i] ? 1 + dp[i][t - coins[i]] : 1e9;
+                dp[i][t] = min(take, not_take);
+            }
+        }
+
+        return dp[n - 1][amount] == 1e9 ? -1 : dp[n - 1][amount];
+    }
+    /*
     int f(int ind, int target, vector<int>& arr, vector<vector<int>>& dp) {
         if (ind == 0) {
             // can first coin be used for remaining amount
@@ -27,4 +53,5 @@ public:
         int minCoins = f(ind, amount, coins, dp);
         return (minCoins >= 1e9) ? -1 : minCoins;
     }
+    */
 };
