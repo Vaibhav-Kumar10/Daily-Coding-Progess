3013c3d2207c952276f69f9169e6636bf0439d47
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-24 19:09:39 +0530

    606 Construct String from Binary Tree - Runtime: 0 ms, Memory: 24.4 MB

---
+++ 606-construct-string-from-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void preorder(TreeNode* root, string& ans) {
+        if (root == NULL) {
+            return;
+        }
+        ans += to_string(root->val);
+        TreeNode* left_child = root->left;
+        TreeNode* right_child = root->right;
+        if (left_child != NULL) {
+            ans += '(';
+            preorder(left_child, ans);
+            ans += ')';
+        }
+        if (right_child != NULL) {
+            if (left_child == NULL) {
+                ans += '(';
+                ans += ')';
+            }
+            ans += '(';
+            preorder(right_child, ans);
+            ans += ')';
+        }
+    }
+    string tree2str(TreeNode* root) {
+        string ans = "";
+        preorder(root, ans);
+        return ans;
+    }
+};
