c50cb7bb7240cf96485df7b31d18b22fb792ee59
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-24 17:59:27 +0530

    1631 Path With Minimum Effort - Runtime: 98 ms, Memory: 44 MB

--- 1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort.cpp
@@ -4,6 +4,36 @@ public:
         int n = heights.size(), m = heights[0].size();
         vector<int> dr = {0, 1, 0, -1}, dc = {1, 0, -1, 0};
         vector<vector<int>> efforts(n, vector<int>(m, INT_MAX));
+        queue<tuple<int, int, int>> q;
+        // { cur_effort, row, col }
+        q.push({0, 0, 0});
+        efforts[0][0] = 0;
+        while (!q.empty()) {
+            auto [cur_effort, row, col] = q.front();
+            q.pop();
+            if (cur_effort > efforts[row][col]) {
+                continue;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nr = row + dr[i], nc = col + dc[i];
+                if (nr < 0 || nc < 0 || nr >= n || nc >= m) {
+                    continue;
+                }
+                int next_effort = abs(heights[row][col] - heights[nr][nc]);
+                int maxEffort = max(cur_effort, next_effort);
+                if (maxEffort < efforts[nr][nc]) {
+                    efforts[nr][nc] = maxEffort;
+                    q.push({efforts[nr][nc], nr, nc});
+                }
+            }
+        }
+        return efforts[n - 1][m - 1];
+    }
+    /*
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n = heights.size(), m = heights[0].size();
+        vector<int> dr = {0, 1, 0, -1}, dc = {1, 0, -1, 0};
+        vector<vector<int>> efforts(n, vector<int>(m, INT_MAX));
         priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
                        greater<tuple<int, int, int>>>
             pq;
@@ -34,4 +64,5 @@ public:
         }
         return 0;
     }
+    */
 };
