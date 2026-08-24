c8e4580880ac198bd1f6f1e61ffb618a8729a2ca
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-25 00:03:45 +0530

    2283 Check if Number Has Equal Digit Count and Digit Value - Runtime: 0 ms, Memory: 8 MB

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool digitCount(string num) {
+        vector<int> freq(10, 0);
+        int n = num.size();
+        for (int i = 0; i < n; i++) {
+            freq[num[i] - '0']++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (freq[i] != num[i] - '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+};
