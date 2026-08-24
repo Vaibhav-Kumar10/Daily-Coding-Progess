d567758a4a023cc694bb3b566b9b96590a73c804
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-24 23:22:42 +0530

    507 Perfect Number - Runtime: 1894 ms, Memory: 7.7 MB

--- 507-perfect-number.cpp
+++ 507-perfect-number.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
         int divisor_sum = 0;
         for (int divisor = 1; divisor < num; divisor++) {
             if (num % divisor == 0) {
                 divisor_sum += divisor;
+            }
+            if (divisor_sum > num) {
+                return false;
             }
         }
         return divisor_sum == num;
