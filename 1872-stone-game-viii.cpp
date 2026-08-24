ebfd78577561866d22ba8774256b47110ca87194
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-24 11:23:57 +0530

    1872 Stone Game VIII - Runtime: 80 ms, Memory: 95.1 MB

--- 1872-stone-game-viii.cpp
+++ 1872-stone-game-viii.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        vector<int> prefixSum_score(n, 0);
+        // When taking i + 1 stones => player gets the score
+        // directly using prefoxSum => bcz, it stores the sum of all prev stones
+        prefixSum_score[0] = stones[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum_score[i] = prefixSum_score[i - 1] + stones[i];
+        }
+
+        vector<int> dp(n, 0);
+        dp[n - 1] = prefixSum_score[n - 1];
+
+        for (int ind = n - 2; ind > 0; ind--) {
+            int player_take = prefixSum_score[ind];
+            int opponent_score = dp[ind + 1];
+            int score_dif_after_take = player_take - opponent_score;
+
+            int player_skip = dp[ind + 1];
+            dp[ind] = max(player_skip, score_dif_after_take);
+        }
+        return dp[1];
+    }
+    /*
     int f(int ind, vector<int>& prefixSum_score, int n, vector<int>& dp) {
         if (ind == n - 1) {
             return prefixSum_score[ind];
@@ -26,4 +50,5 @@ public:
         // Start with Alice
         return f(1, prefixSum_score, n, dp);
     }
+    */
 };
