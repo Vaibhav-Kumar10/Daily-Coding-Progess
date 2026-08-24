c89217feed6261e0b1a19500e531e3af99696fab
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-25 00:37:00 +0530

    2180 Count Integers With Even Digit Sum - Runtime: 0 ms, Memory: 7.9 MB

--- 2180-count-integers-with-even-digit-sum.cpp
+++ 2180-count-integers-with-even-digit-sum.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int countEven(int num) {
+        int digit_sum = 0, original_num = num;
+        while (num > 0) {
+            int digit = num % 10;
+            num /= 10;
+            digit_sum += digit;
+        }
+        // if cur num -> sum of digits is even
+        // => num / 2 no. have even sum
+        //
+        // if cur num -> sum of digits is odd
+        // => (num - 1) / 2 no. have even sum
+        //
+        if (digit_sum % 2 == 0) {
+            return original_num / 2;
+        }
+        return (original_num - 1) / 2;
+    }
+    /*
+    int countEven(int num) {
         int cnt = 0;
         for (int no = 1; no <= num; no++) {
             int digit_sum = 0, n = no;
@@ -15,4 +34,5 @@ public:
         }
         return cnt;
     }
+    */
 };
