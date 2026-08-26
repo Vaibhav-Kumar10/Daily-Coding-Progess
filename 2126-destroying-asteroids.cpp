1421b374632f3ac087c19a3ceaf71687216384db
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-26 23:44:07 +0530

    2126 Destroying Asteroids - Runtime: 34 ms, Memory: 106.8 MB

---
+++ 2126-destroying-asteroids.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        long long planet_mass = mass;
+        for (int asteroid : asteroids) {
+            if (planet_mass < asteroid) {
+                return false;
+            }
+            planet_mass += asteroid;
+        }
+        return true;
+    }
+};
