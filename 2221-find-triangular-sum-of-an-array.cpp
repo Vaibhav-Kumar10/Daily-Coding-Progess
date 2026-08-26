8a627786885abbbd01d73ed30773c3c98cc74792
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-27 00:04:45 +0530

    2221 Find Triangular Sum of an Array - Runtime: 427 ms, Memory: 384.6 MB

---
+++ 2221-find-triangular-sum-of-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0] % 10;
+        }
+        vector<int> arr;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            arr.push_back((nums[i] + nums[i + 1]) % 10);
+        }
+        return triangularSum(arr) % 10;
+    }
+};
