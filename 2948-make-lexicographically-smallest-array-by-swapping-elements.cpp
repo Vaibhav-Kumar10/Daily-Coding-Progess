a852411edb2f36a6a28c95ed23a0f14d9b73e7ed
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-31 11:17:56 +0530

    2948 Make Lexicographically Smallest Array by Swapping Elements - Runtime: 335 ms, Memory: 360.9 MB

--- 2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,47 +1,57 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        vector<int> arr(nums.begin(), nums.end());
+        sort(arr.begin(), arr.end());
         int n = nums.size();
 
-        vector<pair<int, int>> a;
+        unordered_map<int, queue<int>> grp_els;
+        unordered_map<int, int> grp_no;
+
+        int grp = 0;
+        grp_no[arr[0]] = grp;
+        grp_els[grp].push(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - arr[i - 1] <= limit) {
+                grp_els[grp].push(arr[i]);
+                grp_no[arr[i]] = grp;
+            } else {
+                grp++;
+                grp_els[grp].push(arr[i]);
+                grp_no[arr[i]] = grp;
+            }
+        }
+
         for (int i = 0; i < n; i++) {
-            a.push_back({nums[i], i});
+            int num = nums[i];
+            int grp = grp_no[num];
+            int first_from_grp = grp_els[grp].front();
+            grp_els[grp].pop();
+            nums[i] = first_from_grp;
         }
 
-        // Sort by value
-        sort(a.begin(), a.end());
-
-        vector<int> ans = nums;
-
-        int start = 0;
-
-        while (start < n) {
-            int end = start;
-
-            // Find all values connected through swaps
-            while (end + 1 < n &&
-                   a[end + 1].first - a[end].first <= limit) {
-                end++;
-            }
-
-            // Values in this group are already sorted.
-            // Collect their original indices.
-            vector<int> indices;
-
-            for (int i = start; i <= end; i++) {
-                indices.push_back(a[i].second);
-            }
-
-            // Smallest values should go to smallest indices.
-            sort(indices.begin(), indices.end());
-
-            for (int i = start; i <= end; i++) {
-                ans[indices[i - start]] = a[i].first;
-            }
-
-            start = end + 1;
-        }
-
-        return ans;
+        return nums;
     }
+    /*
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size(), i = 0;
+        while (i < n) {
+            int swap_idx = -1, smallest_swap_option = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] > nums[j] && abs(nums[i] - nums[j]) <= limit) {
+                    if (nums[j] < smallest_swap_option) {
+                        swap_idx = j;
+                        smallest_swap_option = nums[j];
+                    }
+                }
+            }
+            if (swap_idx != -1) {
+                swap(nums[i], nums[swap_idx]);
+            } else {
+                i++;
+            }
+        }
+        return nums;
+    }
+    */
 };
