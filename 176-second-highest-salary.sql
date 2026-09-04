f2bc5009b468ff2e406884a45a4667155002270c
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-09-04 10:44:42 +0530

    176 Second Highest Salary - Runtime: 285 ms, Memory: 0B

--- 176-second-highest-salary.sql
+++ 176-second-highest-salary.sql
@@ -1,6 +1,14 @@
+SELECT (
+    SELECT DISTINCT salary 
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+/*
 SELECT MAX(salary) as SecondHighestSalary
 FROM Employee
 WHERE salary < (
     SELECT MAX(salary) 
     FROM Employee
 );
+*/
