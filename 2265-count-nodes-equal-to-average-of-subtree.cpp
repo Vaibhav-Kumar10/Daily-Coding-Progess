cdb356ab2080305eac6b93b94aa1c2d19cd51363
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-09-10 19:18:06 +0530

    2265 Count Nodes Equal to Average of Subtree - Runtime: 3 ms, Memory: 15.8 MB

--- 2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -12,6 +12,31 @@
  */
 class Solution {
 public:
+    int ans = 0;
+    pair<int, int> f(TreeNode* root) {
+        if (root == NULL) {
+            return {0, 0};
+        }
+        pair<int, int> left_data = f(root->left);
+        pair<int, int> right_data = f(root->right);
+        int child_sum = left_data.first + right_data.first;
+        int child_cnt = left_data.second + right_data.second;
+
+        int total_sum = child_sum + root->val, total_cnt = child_cnt + 1;
+        int avg = total_sum / total_cnt;
+        if (avg == root->val) {
+            ans++;
+        }
+        return {total_sum, total_cnt};
+    }
+    int averageOfSubtree(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        f(root);
+        return ans;
+    }
+    /*
     int f(TreeNode* root, int& cnt) {
         if (root == NULL) {
             return 0;
@@ -34,4 +59,5 @@ public:
         averageOfSubtree(root->right);
         return ans;
     }
+    */
 };
