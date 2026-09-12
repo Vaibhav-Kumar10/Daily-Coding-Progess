0b0e62fad1c9a4ec9e02ee30aecad710555c5ce4
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-09-12 16:19:26 +0530

    3483 Unique 3-Digit Even Numbers - Runtime: 7 ms, Memory: 30.9 MB

--- 3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int totalNumbers(vector<int>& digits) {
+        int n = digits.size();
+        unordered_set<int> numbers;
+        for (int hundreds = 0; hundreds < n; hundreds++) {
+            for (int tens = 0; tens < n; tens++) {
+                for (int ones = 0; ones < n; ones++) {
+                    if (ones == tens || ones == hundreds || tens == hundreds) {
+                        continue;
+                    }
+                    int num = digits[hundreds] * 100 + digits[tens] * 10 +
+                              digits[ones];
+                    if (num >= 100 && num % 2 == 0) {
+                        numbers.insert(num);
+                    }
+                }
+            }
+        }
+        return numbers.size();
+        ;
+    }
+    /*
     void f(string cur, vector<int>& digits, set<int>& numbers, int n,
            vector<bool>& visited) {
         if (cur.size() == 3) {
@@ -29,4 +50,5 @@ public:
         f(num, digits, numbers, n, visited);
         return numbers.size();
     }
+    */
 };
