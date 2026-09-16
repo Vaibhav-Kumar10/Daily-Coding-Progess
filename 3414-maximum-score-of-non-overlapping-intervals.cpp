e1a1b92d6bda0acdcc5b5023567d0d9708c5b5bf
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-09-16 15:49:04 +0530

    3414 Maximum Score of Non-overlapping Intervals - Runtime: 1225 ms, Memory: 387.5 MB

--- 3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -18,6 +18,74 @@ public:
         }
         return ans;
     }
+    vector<int> maximumWeight(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        for (int i = 0; i < n; i++) {
+            intervals[i].push_back(i);
+        }
+        vector<int> next_intervals(n);
+        sort(intervals.begin(), intervals.end());
+        for (int i = 0; i < n; i++) {
+            int cur_end = intervals[i][1];
+            next_intervals[i] = findNext(cur_end, intervals, n);
+        }
+        vector<vector<Score_Node>> dp(n + 1, vector<Score_Node>(5));
+
+        for (int ind = 0; ind <= n; ind++) {
+            dp[ind][0].score = 0;
+        }
+        for (int k = 0; k < 5; k++) {
+            dp[n][k].score = 0;
+        }
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int k = 1; k < 5; k++) {
+                // skip cur interval
+                Score_Node skip = dp[ind + 1][k];
+
+                // take cur interval
+                int original_index = intervals[ind][3];
+                int next_idx = next_intervals[ind];
+                Score_Node take = dp[next_idx][k - 1];
+
+                take.score = intervals[ind][2] + take.score;
+                take.indices_taken.push_back(original_index);
+                sort(begin(take.indices_taken), end(take.indices_taken));
+
+                Score_Node max_ans;
+                if (skip.score > take.score) {
+                    max_ans = skip;
+                } else if (skip.score < take.score) {
+                    max_ans = take;
+                } else {
+                    max_ans =
+                        skip.indices_taken < take.indices_taken ? skip : take;
+                }
+                dp[ind][k] = max_ans;
+            }
+        }
+        Score_Node ans_node = dp[0][4];
+        return ans_node.indices_taken;
+    }
+    /*
+    class Score_Node {
+    public:
+        long long score = -1;
+        vector<int> indices_taken;
+    };
+    int findNext(int endpoint, vector<vector<int>>& intervals, int n) {
+        int low = 0, high = n - 1, ans = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (intervals[mid][0] > endpoint) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
     Score_Node f(int ind, int k, vector<vector<int>>& intervals,
                  vector<int>& next_intervals, int n,
                  vector<vector<Score_Node>>& dp) {
@@ -67,4 +135,5 @@ public:
         Score_Node ans_node = f(ind, k, intervals, next_intervals, n, dp);
         return ans_node.indices_taken;
     }
+    */
 };
