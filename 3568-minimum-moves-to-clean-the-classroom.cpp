80a5ed7fd53a9862a14e0e89f96cc6be49ede6d5
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-09-17 12:15:24 +0530

    3568 Minimum Moves to Clean the Classroom - Runtime: 740 ms, Memory: 242.1 MB

--- 3568-minimum-moves-to-clean-the-classroom.cpp
+++ 3568-minimum-moves-to-clean-the-classroom.cpp
@@ -1,56 +1,67 @@
 class Solution {
-    int dx[4] = {0, 1, 0, -1};
-    int dy[4] = {1, 0, -1, 0};
-
 public:
+    struct State {
+        int row, col, e, lbm;
+    };
     int minMoves(vector<string>& classroom, int energy) {
-        int m = classroom.size(), n = classroom[0].size();
-        vector id(m, vector<int>(n));
-        int sx, sy, cnt = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int n = classroom.size(), m = classroom[0].size();
+        map<pair<int, int>, int> litter_pos;
+        int litter_cnt = 0, start_row, start_col;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (classroom[i][j] == 'S') {
-                    sx = i;
-                    sy = j;
+                    start_row = i;
+                    start_col = j;
                 } else if (classroom[i][j] == 'L') {
-                    id[i][j] = 1 << cnt++;
+                    litter_pos[{i, j}] = litter_cnt;
+                    litter_cnt++;
                 }
             }
         }
-
-        vector bestEnergy(m, vector(n, vector<int>(1 << cnt, -1)));
-        bestEnergy[sx][sy][0] = energy;
-        struct Info {
-            int x, y, mask, e, steps;
-        };
-        queue<Info> q;
-        q.push({sx, sy, 0, energy, 0});
+        if (litter_cnt == 0) {
+            return 0;
+        }
+        vector<int> dr = {0, 1, 0, -1}, dc = {1, 0, -1, 0};
+        int total_litter_mask = (1 << litter_cnt) - 1;
+        queue<State> q;
+        bool visited[n + 1][m + 1][energy + 1][total_litter_mask + 1];
+        memset(visited, false, sizeof(visited));
+        q.push({start_row, start_col, energy, 0});
+        visited[start_row][start_col][energy][0] = true;
+        int moves = 0;
         while (!q.empty()) {
-            Info t = q.front();
-            q.pop();
-            if (t.mask == (1 << cnt) - 1) {
-                return t.steps;
-            }
-            if (t.e == 0) {
-                continue;
-            }
-            for (int i = 0; i < 4; i++) {
-                int nx = t.x + dx[i];
-                int ny = t.y + dy[i];
-
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n ||
-                    classroom[nx][ny] == 'X') {
+            int q_size = q.size();
+            while (q_size--) {
+                auto cur_state = q.front();
+                q.pop();
+                if (cur_state.lbm == total_litter_mask) {
+                    return moves;
+                }
+                if (cur_state.e == 0) {
                     continue;
                 }
-
-                int ne = classroom[nx][ny] == 'R' ? energy : t.e - 1;
-                int nmask = t.mask | id[nx][ny];
-
-                if (ne > bestEnergy[nx][ny][nmask]) {
-                    bestEnergy[nx][ny][nmask] = ne;
-                    q.push({nx, ny, nmask, ne, t.steps + 1});
+                for (int i = 0; i < 4; i++) {
+                    int nr = cur_state.row + dr[i], nc = cur_state.col + dc[i];
+                    if (nr < 0 || nc < 0 || nr >= n || nc >= m ||
+                        classroom[nr][nc] == 'X') {
+                        continue;
+                    }
+                    char next_cell = classroom[nr][nc];
+                    int next_energy = cur_state.e - 1;
+                    int next_mask = cur_state.lbm;
+                    if (next_cell == 'R') {
+                        next_energy = energy;
+                    } else if (next_cell == 'L') {
+                        int next_litter_pos = litter_pos[{nr, nc}];
+                        next_mask |= (1 << next_litter_pos);
+                    }
+                    if (!visited[nr][nc][next_energy][next_mask]) {
+                        visited[nr][nc][next_energy][next_mask] = true;
+                        q.push({nr, nc, next_energy, next_mask});
+                    }
                 }
             }
+            moves++;
         }
         return -1;
     }
