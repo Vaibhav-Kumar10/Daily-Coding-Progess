637b3dbf15880e6a412216163e1e034e981368b0
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-09-18 21:47:00 +0530

    1520 Maximum Number of Non-Overlapping Substrings - Runtime: 24 ms, Memory: 23.3 MB

---
+++ 1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int checkSubstr(string& s, int i, vector<int>& l, vector<int>& r) {
+        int right = r[s[i] - 'a'];
+        for (auto j = i; j <= right; ++j) {
+            if (l[s[j] - 'a'] < i) {
+                return -1;
+            }
+            right = max(right, r[s[j] - 'a']);
+        }
+        return right;
+    }
+    vector<string> maxNumOfSubstrings(string s) {
+        vector<int> l(26, INT_MAX), r(26, INT_MIN);
+        vector<string> res;
+        for (int i = 0; i < s.size(); ++i) {
+            l[s[i] - 'a'] = min(l[s[i] - 'a'], i);
+            r[s[i] - 'a'] = i;
+        }
+        int right = -1;
+        for (int i = 0; i < s.size(); ++i) {
+            if (i == l[s[i] - 'a']) {
+                int new_right = checkSubstr(s, i, l, r);
+                if (new_right != -1) {
+                    if (i > right) {
+                        res.push_back("");
+                    }
+                    right = new_right;
+                    res.back() = s.substr(i, right - i + 1);
+                }
+            }
+        }
+        return res;
+    }
+};
+// class Solution {
+// public:
+//     vector<string> maxNumOfSubstrings(string s) {
+//         unordered_map<char, int> charFreq;
+//         for (char ch : s) {
+//             charFreq[ch]++;
+//         }
+//         vector<string> ans;
+//         for (auto [ch, freq] : charFreq) {
+//             if (freq == 1) {
+//                 ans.push_back(ch);
+//             }
+//         }
+//         for (int i = 1; i < n; i++) {
+//             if (s[i] != s[i - 1]) {
+//             }
+//         }
+//         return ans;
+//     }
+// };
