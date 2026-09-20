77cc383b156fee2c0efaafebe394a01543748e73
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-09-20 17:17:25 +0530

    3498 Reverse Degree of a String - Runtime: 0 ms, Memory: 9.8 MB

---
+++ 3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0, n = s.size();
+        for (int i = 0; i < n; i++) {
+            ans += (26 - s[i] + 'a') * (i + 1);
+        }
+        return ans;
+    }
+};
