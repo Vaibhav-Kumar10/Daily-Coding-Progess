f20dbe200a3278c8753c420e20c9e19b3b433fa2
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-09-24 16:39:57 +0530

    1658 Minimum Operations to Reduce X to Zero - Runtime: 186 ms, Memory: 157.3 MB

--- 1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int minOperations(vector<int>& A, int x) {
-        int k = reduce(A.begin(), A.end()) - x, n = A.size();
-        if (k < 0) return -1;
-        if (k == 0) return n;
-
-        int best = -1, i = 0, sum = 0;
-        for (int j = 0; j < n; j++) {
-            sum += A[j];
-            while (sum > k)
-                sum -= A[i++];
-
-            if (sum == k)
-                best = max(best, j - i + 1);
+    int minOperations(vector<int>& nums, int x) {
+        unordered_map<int, int> prevOccIdx;
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int target = total - x, n = nums.size(), maxLen = -1, cur_sum = 0;
+        prevOccIdx[0] = -1;
+        if (target == 0) {
+            return n;
         }
-
-        return best + 1 ? n - best : -1;
+        for (int i = 0; i < n; i++) {
+            cur_sum += nums[i];
+            int req = cur_sum - target;
+            if (prevOccIdx.find(req) != prevOccIdx.end()) {
+                maxLen = max(maxLen, i - prevOccIdx[req]);
+            }
+            if (prevOccIdx.find(cur_sum) == prevOccIdx.end()) {
+                prevOccIdx[cur_sum] = i;
+            }
+        }
+        if (maxLen == -1) {
+            return -1;
+        }
+        return n - maxLen;
     }
 };
